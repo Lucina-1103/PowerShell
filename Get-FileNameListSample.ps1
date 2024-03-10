abeab7134f414bd00e9206ebd0f21c8f6c4beab3
Lucina <lucina.tech@gmail.com>
2024-03-10 11:35:35 +0900

    add-Get-FileNameListSample

---
+++ Get-FileNameListSample.ps1
@@ -0,0 +1,9 @@
+﻿# Get-FileNameListSample
+# 指定されたパスに格納されたファイルのファイル名を取得します。
+
+$path = 'C:\Users\xxxxx\xxxxx\xxxxx' # 参照するパス
+
+$items = Get-childItem $path -File
+foreach($item in $items) {
+    $item.name
+}
